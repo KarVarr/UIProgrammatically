ebd3fab3281ca36ffa92fe19c93df2cacba6aca2
KarVarr <megatr9n@gmail.com>
2023-01-25 20:22:27 +0300

    added button programmatically. use NSLayoutConstraint.active

--- TestApp/TestApp/ViewController.swift
+++ TestApp/TestApp/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(.magenta, for: .normal)
+        button.setTitleShadowColor(.black, for: .normal)
+        button.backgroundColor = .systemIndigo
+        button.layer.cornerRadius = 10
+        
+        
+        return button
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .magenta
+        view.backgroundColor = .white
+        
+        setupView()
     }
+    
+   private func setupView() {
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15)
+        ])
+    }
+    
+    
+    
 
 
 }
