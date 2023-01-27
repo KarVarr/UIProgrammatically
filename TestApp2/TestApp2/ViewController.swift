489c4d1b8fb80f10543caf1ad66c320cf6219d27
KarVarr <megatr9n@gmail.com>
2023-01-27 18:20:18 +0300

    added testApp2

---
+++ TestApp2/TestApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp2
+//
+//  Created by Karen Vardanian on 27.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
