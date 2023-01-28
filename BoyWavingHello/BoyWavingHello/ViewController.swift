f9b9a580ca1faea041f6bc6ee388202757882a31
KarVarr <megatr9n@gmail.com>
2023-01-28 21:11:30 +0300

    added image from project and finished the boy project

--- BoyWavingHello/BoyWavingHello/ViewController.swift
+++ BoyWavingHello/BoyWavingHello/ViewController.swift
@@ -9,6 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let horizontalStack:  UIStackView = {
+       let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.layer.borderColor = UIColor.red.cgColor
+        stack.layer.borderWidth = 1
+        stack.backgroundColor = .red
+        return stack
+    }()
+    
+    let menuImage: UIImageView = {
+       let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "menu")
+        
+        return image
+    }()
+    
+    let skipButton: UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Skip", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .clear
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 17
+        return button
+    }()
+    
+    
     let image: UIImageView = {
         var image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
@@ -43,6 +74,8 @@ class ViewController: UIViewController {
         dot.backgroundStyle = .automatic
         dot.numberOfPages = 3
         dot.currentPage = 0
+        dot.tintColor = .white
+        dot.currentPageIndicatorTintColor = .darkGray
         return dot
     }()
 
@@ -50,10 +83,48 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor(red: 0.82, green: 0.86, blue: 0.97, alpha: 1.00)
+        setupHorizontal()
+        setupMenuImage()
+        setupButton()
         setupImage()
         setupTitleLabel()
         setupLoremLabel()
         setupDot()
+    }
+    
+    func setupHorizontal() {
+        view.addSubview(horizontalStack)
+        [menuImage, skipButton].forEach {
+            horizontalStack.addArrangedSubview($0)
+        }
+        NSLayoutConstraint.activate([
+            horizontalStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            horizontalStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            horizontalStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            horizontalStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 16),
+            horizontalStack.widthAnchor.constraint(equalToConstant: 350)
+            
+        ])
+    }
+    
+    func setupMenuImage() {
+        view.addSubview(menuImage)
+        NSLayoutConstraint.activate([
+            menuImage.widthAnchor.constraint(equalToConstant: 35),
+            menuImage.heightAnchor.constraint(equalToConstant: 35),
+            menuImage.topAnchor.constraint(equalTo: horizontalStack.topAnchor),
+            menuImage.leadingAnchor.constraint(equalTo: horizontalStack.leadingAnchor, constant: 20)
+        ])
+    }
+    
+    func setupButton() {
+        view.addSubview(skipButton)
+        NSLayoutConstraint.activate([
+            skipButton.widthAnchor.constraint(equalToConstant: 100),
+            skipButton.trailingAnchor.constraint(equalTo: horizontalStack.trailingAnchor, constant: -45),
+            skipButton.topAnchor.constraint(equalTo: horizontalStack.topAnchor),
+            
+        ])
     }
     
     func setupImage()  {
