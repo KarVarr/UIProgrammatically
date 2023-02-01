094e039c69681685db2f335c7267f379a3228baf
KarVarr <megatr9n@gmail.com>
2023-02-01 19:01:31 +0300

    added resources Colors

---
+++ WorkoutApp/WorkoutApp/Helpers/Resources.swift
@@ -0,0 +1,17 @@
+//
+//  Resources.swift
+//  WorkoutApp
+//
+//  Created by Karen Vardanian on 01.02.2023.
+//
+
+import UIKit
+
+enum Resources {
+    enum Colors {
+        static var active = UIColor(hexString: "#437BFE")
+        static var inActive = UIColor(hexString: "#929DA5")
+        
+        static var separator = UIColor(hexString: "#E8ECEF")
+    }
+}
--- WorkoutApp/WorkoutApp/TabBarController.swift
+++ WorkoutApp/WorkoutApp/TabBarController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 enum Tabs: Int {
-case overview
-case session
-case progress
-case settings
+    case overview
+    case session
+    case progress
+    case settings
 }
 
 final class TabBarController: UITabBarController {
     private func configure() {
+        tabBar.tintColor = Resources.Colors.active
+        tabBar.barTintColor = Resources.Colors.inActive
+        tabBar.backgroundColor = .white
         
+        tabBar.layer.borderColor = Resources.Colors.separator.cgColor
+        tabBar.layer.borderWidth = 1
+        tabBar.layer.masksToBounds = true
     }
 }
