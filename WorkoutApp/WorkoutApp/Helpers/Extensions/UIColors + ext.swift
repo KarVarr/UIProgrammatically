3c9340f6461b2246459b1bf47f747c6d51b88945
KarVarr <megatr9n@gmail.com>
2023-02-01 18:50:29 +0300

    added folders Helpers Application SupportingFiles and remove the storyboard

---
+++ WorkoutApp/WorkoutApp/Helpers/Extensions/UIColors + ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIColors + ext.swift
+//  WorkoutApp
+//
+//  Created by Karen Vardanian on 01.02.2023.
+//
+
+import Foundation
---
+++ WorkoutApp/WorkoutApp/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  WorkoutApp
+//
+//  Created by Karen Vardanian on 01.02.2023.
+//
+
+import UIKit
+
+enum Tabs: Int {
+case overview
+case session
+case progress
+case settings
+}
+
+final class TabBarController: UITabBarController {
+    private func configure() {
+        
+    }
+}
