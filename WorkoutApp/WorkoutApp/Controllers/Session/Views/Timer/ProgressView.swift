02b35aa03d3f0b220133de4b57e38a588231864d
KarVarr <megatr9n@gmail.com>
2023-02-06 20:39:05 +0300

    added progressview

--- WorkoutApp/WorkoutApp/Controllers/Session/Views/Timer/ProgressView.swift
+++ WorkoutApp/WorkoutApp/Controllers/Session/Views/Timer/ProgressView.swift
@@ -11,6 +11,25 @@ extension TimerView {
     final class ProgressView: UIView {
         func drawProgress(with percent: CGFloat) {
             
+            let circleFrame = UIScreen.main.bounds.width - (15 + 40) * 2
+            let radius = circleFrame / 2
+            let center = CGPoint(x: radius, y: radius)
+            let startAngel = -CGFloat.pi * 7 / 6
+            let endAngel = CGFloat.pi * 1 / 6
+            
+            let circlePath = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngel, endAngle: endAngel, clockwise: true)
+            
+            
+            
+            let circleLayer = CAShapeLayer()
+            circleLayer.path = circlePath.cgPath
+            circleLayer.strokeColor = R.Colors.active.cgColor
+            circleLayer.lineWidth = 20
+            circleLayer.strokeEnd = percent
+            circleLayer.fillColor = UIColor.clear.cgColor
+            circleLayer.lineCap = .round
+            
+            layer.addSublayer(circleLayer)
         }
     }
 }
