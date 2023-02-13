d2f99df644112a52835a4b29885c6165570902f7
KarVarr <megatr9n@gmail.com>
2023-02-13 19:28:04 +0300

    added NSLayout and configure for titles

--- TemperatureСonversion/TemperatureСonversion/ViewController.swift
+++ TemperatureСonversion/TemperatureСonversion/ViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let titleSegment: UILabel = {
+      let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .gray
+        label.textAlignment = .left
+        return label
+   }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemTeal
+        
+        addView()
+        layout()
+        configure()
+    }
+    
+    func addView() {
+        view.addSubview(titleSegment)
+    }
+    
+    
+    func layout() {
+        NSLayoutConstraint.activate([
+            titleSegment.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleSegment.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15)
+        ])
+    }
+    
+    func configure() {
+        titleSegment.text = "Fahrenheit or celsius".uppercased()
     }
 
 
