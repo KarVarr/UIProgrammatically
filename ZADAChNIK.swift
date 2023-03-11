f00585a5097d059209c5d81762f72904b23fc945
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-03-11 12:32:51 +0300

    update ZADAChNIK

--- ZADAChNIK.swift
+++ ZADAChNIK.swift
@@ -48,6 +48,41 @@ let devideBy = a / b // 0
 let calc = "\(a) + \(b) = \(plus)" // 55 + 88 = 143
 
 
+//2. Строки
+//1) Напишите с помощью строк своё резюме: имя, фамилия, возраст, где живете, хобби и т.п. 
 
+let name = "Karen"
+let lastName = "Vardanian"
+let age = 34
+let city = "Moscow"
+let hobbie = "fitnes"
+
+//2) Соберите из этих строк 1 большую (вспоминаем интерполяцию) и выведите в консоль.
+let aboutMe = "My name is \(name) \(lastName), I'm \(age) years old. I living in the city, named \(city), and my hobbie is \(hobbie)"
+
+// 3) Напишите 10 строк, соберите их с помощью интерполяции и поставьте в нужных местах 
+//переносы на новую строку и пробелы (см. \n и \t).
+
+let story1 = "In fields of green and skies of blue"
+let story2 = "The world is vast and full of hue."
+let story3 = "The flowers sway and dance in breeze,"
+let story4 = "A symphony of nature's ease."
+
+let fullPoem = "\(story1)\n \(story2)\n \t\(story3)\n \t\(story4)"
+
+//4) Разбейте собственное имя на символы, перенося каждую букву на новую строку.
+var myNewName = ""
+let myName = "Karen"
+for i in myName {
+    myNewName += "\(i)\n"
+}
+
+// 5) Создайте переменную типа Int и переменную типа String. 
+// Тип Int преобразуйте в String и с помощью бинарного оператора сложите 2 переменные в одну 
+// строку.
+
+let num1 = 5
+let num2 = "num is: "
+let sumNum =  num2 + String(num1) 
 
 
