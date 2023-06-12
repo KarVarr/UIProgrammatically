3158349debafc113f6225fa7a6679bd7aabc115b
KarVarr <megatr9n@gmail.com>
2023-06-12 23:07:01 +0300

    create test app for massage

---
+++ MassageApp/MassageApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  MassageApp
+//
+//  Created by Karen Vardanian on 12.06.2023.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    
+    @StateObject private var viewModel = MassageViewModel()
+    
+    var body: some View {
+        VStack {
+           
+
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MassageApp/MassageApp/MassageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MassageAppApp.swift
+//  MassageApp
+//
+//  Created by Karen Vardanian on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MassageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
