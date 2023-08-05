9cdf621d983465d7b1df581e6a495c75875431c6
KarVarr <megatr9n@gmail.com>
2023-08-06 01:16:09 +0400

    added collections

--- MyPlaygroundSwiftDoc.playground/Contents.swift
+++ MyPlaygroundSwiftDoc.playground/Contents.swift
@@ -108,59 +108,114 @@ import UIKit
 //userClass2.printName()
 
 
-class Human {
-    static let species: String = "homo sapiens"
-    var weight: Double
-    var height: Double {
-        willSet {
-            weight += 1
-            print("this is WILLSET \(newValue)")
-        }
-        didSet {
-            print("this is DIDSET \(oldValue)")
-        }
-    }
-    
-    var iq: Double {
-        get{
-            print("test for IQ is completed")
-            return Double.random(in: 90...180)
+//class Human {
+//    static let species: String = "homo sapiens"
+//    var weight: Double
+//    var height: Double {
+//        willSet {
+//            weight += 1
+//            print("this is WILLSET \(newValue)")
+//        }
+//        didSet {
+//            print("this is DIDSET \(oldValue)")
+//        }
+//    }
+//
+//    var iq: Double {
+//        get{
+//            print("test for IQ is completed")
+//            return Double.random(in: 90...180)
+//
+//        }
+//        set {
+//            print("now your IQ is \(newValue)")
+//        }
+//
+//    }
+//
+//    init(weight: Double, height: Double) {
+//        self.weight = weight
+//        self.height = height
+//    }
+//
+//    func lose(weight: Double) {
+//        var result = self.weight - weight
+//        print(result)
+//    }
+//
+//
+//}
+//
+//func foo(f: inout Double){
+//    print("call func")
+//}
+//
+//let bob = Human(weight: 80, height: 180)
+//bob.height
+//bob.weight
+//bob.lose(weight: 5.5)
+//print(bob.iq)
+//bob.iq = 100
+//bob.height = 176
+//
+//foo(f: &bob.weight)
+//bob.weight
+//foo(f: &bob.weight)
+//bob.weight
+//
+//Human.species
 
-        }
-        set {
-            print("now your IQ is \(newValue)")
-        }
-        
-    }
+
+let str = "Hello Boss"
+
+if true {
+    let str = "Goodbye"
     
-    init(weight: Double, height: Double) {
-        self.weight = weight
-        self.height = height
-    }
-    
-    func lose(weight: Double) {
-        var result = self.weight - weight
-        print(result)
-    }
-    
-    
+    print(str)
 }
 
-func foo(f: inout Double){
-    print("call func")
-}
+print(str)
 
-let bob = Human(weight: 80, height: 180)
-bob.height
-bob.weight
-bob.lose(weight: 5.5)
-print(bob.iq)
-bob.iq = 100
-bob.height = 176
 
-foo(f: &bob.weight)
-bob.weight
-foo(f: &bob.weight)
-bob.weight
+let sumStr: (String, String) -> String = {$0 + $1}
 
-Human.species
+sumStr("hello ", "Bob")
+
+
+//let testStr: String? = "hello"
+//print(testStr)
+
+//MARK: - Collections
+
+//array - index value
+//set - value
+//dictionary - key value
+
+
+var arr = ["bananas", "apples", "peach"]
+print(arr, arr.count)
+arr.insert("strawberry", at: 3)
+print(arr, arr.count)
+let delete = arr.remove(at: 0)
+print(delete)
+print(arr)
+
+var array: Array<Int> = Array()
+array.append(444)
+array.append(444)
+array.append(444)
+var arrya2: [Int] = []
+arr.insert("strawberry", at: 3)
+arr.insert("strawberry", at: 4)
+arr.insert("strawberry", at: 5)
+arr.insert("strawberry", at: 6)
+print(arr.capacity)
+print(array.capacity)
+
+
+let set = Set<Int>()
+let countOfAnimal: Set = [1,3,5,6,7,4,6,7,3,5]
+print(countOfAnimal.sorted())
+
+let dict: [String: String] = [:]
+let dict1 = [String: String]()
