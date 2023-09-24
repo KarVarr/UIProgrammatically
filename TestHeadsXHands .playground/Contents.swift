8baefb2e9334c3efc99bbbf01182a9b4e8f55c2a
KarVarr <megatr9n@gmail.com>
2023-09-24 15:52:14 +0400

    created Creature

--- TestHeadsXHands .playground/Contents.swift
+++ TestHeadsXHands .playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+class Creature {
+    var attack: Int
+    var defense: Int
+    var health: Int
+    var damage: Range<Int>
+    
+    init(attack: Int, defense: Int, health: Int, damage: Range<Int>) {
+        self.attack = attack
+        self.defense = defense
+        self.health = health
+        self.damage = damage
+    }
+}
+
+
