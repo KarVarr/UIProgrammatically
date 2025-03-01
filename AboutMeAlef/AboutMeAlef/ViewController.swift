dab247b8bdb530866115c08f8c9b73ee26fa1feb
KarVarr <megatr9n@gmail.com>
2025-03-01 13:50:18 +0400

    AboutMeAlef: added new project

---
+++ AboutMeAlef/AboutMeAlef/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeAlef
+//
+//  Created by Karen Vardanian on 01.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
